28872180db120c7fba21ed512a8c00b130861776
love kumar todawat <lovetodawat15@gmail.com>
2026-02-04 14:13:50 +0530

    use env database url

--- app/db/database.py
+++ app/db/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
 
-#DATABASE_URL = "mysql+pymysql://root:@127.0.0.1:3306/mewar_erp"
-DATABASE_URL = "mysql+pymysql://root:@127.0.0.1:3306/mewar"
-
+#DATABASE_URL = "mysql+pymysql://root:@127.0.0.1:3306/mewar"
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
